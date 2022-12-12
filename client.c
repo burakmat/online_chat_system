d315515565ce4b3fa3e5ffcac15f21601ebb1980
Burak Mat <burakk.mat@gmail.com>
2022-12-12 13:23:51 +0300

    initial_commit

---
+++ client.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main()
+{
+	char request[256];
+	int net_socket = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(9002);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	int status = connect(net_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+	if (status == -1)
+		printf("Connection error!\n");
+	else
+	{
+		printf("Connected to the server successfully!\n");
+		// recv(net_socket, &response, sizeof(response), 0);
+		while (1)
+		{
+
+			read(0, request, 256);
+			int i = 0;
+			while (request[i] != '\n')
+				++i;
+			request[i] = '\0';
+			write(net_socket, request, 256);
+			read(net_socket, request, 256);
+			printf("Server: %s\n", request);
+		}
+	}
+	close(net_socket);
+	return (0);
+}
---
+++ server.c
@@ -0,0 +1,393 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+typedef struct s_messages
+{
+    char message[256];
+    char sending[32];
+    struct s_messages *next;
+}   t_messages;
+
+
+
+typedef struct s_server
+{
+    int last_client_socket;
+    int user_count;
+    char **users;
+    int *number_of_messages;
+    t_messages **messages;
+}   t_server;
+
+void print_users(char **users)
+{
+    int i = 0;
+    while (users && users[i])
+    {
+        printf("'%s'\n", users[i]);
+        ++i;
+    }
+}
+
+static int	ft_numlen(int n)
+{
+	unsigned int	len;
+	long			m;
+
+	m = n;
+	len = 0;
+	if (m < 0)
+	{
+		len++;
+		m *= -1;
+	}
+	while (m > 9)
+	{
+		m /= 10;
+		len++;
+	}
+	return (len + 1);
+}
+
+char	*itoa(int n)
+{
+	char	*number;
+	int		revind;
+	int		misign;
+	long	m;
+
+	m = n;
+	misign = 0;
+	revind = ft_numlen(n);
+	number = (char *)malloc(sizeof(char) * (ft_numlen(n) + 1));
+	if (!number)
+		return (NULL);
+	number[revind] = '\0';
+	if (m < 0)
+	{
+		misign = 1;
+		m *= -1;
+		number[0] = '-';
+	}
+	while (revind-- > misign)
+	{
+		number[revind] = (m % 10) + '0';
+		m /= 10;
+	}
+	return (number);
+}
+
+char *concat_get_message(char *msg_src, char *msg)
+{
+    char *tmp = malloc(sizeof(char) * (strlen(msg_src) + strlen(msg) + 2));
+    int i = 0;
+    while (msg_src[i])
+    {
+        tmp[i] = msg_src[i];
+        ++i;
+    }
+    tmp[i++] = ' ';
+    int j = 0;
+    while (msg[j])
+    {
+        tmp[i + j] = msg[j];
+        ++j; 
+    }
+    tmp[i + j] = '\0';
+    return (tmp);
+}
+
+char *concat_begin_message(char *first, int number_of_message, char *last)
+{
+	char *ascii = itoa(number_of_message);
+	char *str = malloc(sizeof(char) * (strlen(first) + strlen(last) + strlen(ascii) + 1));
+	int i = 0;
+	while (first[i])
+	{
+		str[i] = first[i];
+		++i;
+	}
+	int j = 0;
+	while (ascii[j])
+	{
+		str[i + j] = ascii[j];
+		++j;
+	}
+	int n = 0;
+	while (last[n])
+	{
+		str[i + j + n] = last[n];
+		++n;
+	}
+	str[i + j + n] = '\0';
+	free(ascii);
+	return (str);
+}
+
+int find_user(t_server *server, char *name, int *user_id)
+{
+    if (server->users == NULL) //first_user
+    {
+        ++server->user_count;
+        server->users = malloc(sizeof(char *) * 2);
+        server->users[0] = malloc(sizeof(char) * (strlen(name) + 1));
+        strcpy(server->users[0], name);
+        server->users[1] = NULL;
+        server->number_of_messages = malloc(sizeof(int));
+        server->number_of_messages[0] = 0;
+        server->messages = malloc(sizeof(t_messages *));
+        server->messages[0] = NULL;
+        *user_id = 0;
+        return (0);
+    }
+    else
+    {
+        int i = 0;
+
+        while (server->users[i])
+        {
+            if (strcmp(server->users[i], name) == 0) //already exist user
+            {
+                *user_id = i;
+                return (1);
+            }
+            ++i;
+        }
+        ++server->user_count;
+        char **tmp = malloc(sizeof(char *) * (i + 2));
+        i = 0;
+        while (server->users[i])
+        {
+            tmp[i] = server->users[i];
+            ++i;
+        }
+        tmp[i + 1] = server->users[i];
+        tmp[i] = malloc(sizeof(char) * (strlen(name) + 1));
+        strcpy(tmp[i], name);
+        free(server->users);
+        server->users = tmp;
+
+        int *tmpnum = malloc(sizeof(int) * server->user_count);
+        for (int i = 0; i < server->user_count - 1; ++i)
+            tmpnum[i] = server->number_of_messages[i];
+        free(server->number_of_messages);
+        tmpnum[server->user_count - 1] = 0;
+        server->number_of_messages = tmpnum;
+        t_messages **tmpmsg = malloc(sizeof(t_messages *) * (server->user_count));
+        i = 0;
+        while (i < server->user_count - 1)
+        {
+            tmpmsg[i] = server->messages[i];
+            ++i;
+        }
+        tmpmsg[i] = NULL;
+        free(server->messages);
+        server->messages = tmpmsg;
+        *user_id = i;
+        return (0);
+    }
+}
+
+int read_protocol(int sock, t_server *server, int *user_id)
+{
+    char user_input[256];
+    int buff = read(sock, user_input, 256);
+    if (buff <= 0)
+    {
+        printf("Client disconnected\n");
+        return (1);
+    }
+    if (strncmp(user_input, "BEGIN_SESSION ", 14) == 0)
+    {
+		if (*user_id != -1)
+		{
+			write(sock, "You should end the active session to begin another", 51);
+			return (0);
+		}
+        //strcpy(current->username, user_input + 14) //char *username = strdup(user_input + 14); //free
+        // printf("got the info, username is %s\n", user_input+14);
+        find_user(server, user_input + 14, user_id);
+        // if (find_user(server, user_input + 14, user_id) == 1)
+        for (int i = 0; i < server->user_count; ++i)
+        {
+            if (strcmp(server->users[i], user_input + 14) == 0)
+            {
+                char *response = concat_begin_message("You have ", server->number_of_messages[*user_id], " short messages");
+				write(sock, response, strlen(response) + 1);
+				free(response);
+                // char *ascii = itoa(server->number_of_messages[i]);
+                // write(sock, "You already logged in.\nYou have ", 33);
+                // write(socket, ascii, strlen(ascii));
+                // write(socket, " short messages", 14);
+                // free(ascii);
+                return 0;
+            }
+        }
+        // else
+        // {
+        //     for (int i = 0; i < server->user_count; ++i)
+        //     {
+        //         if (strcmp(server->users[i], user_input + 14) == 0)
+        //         {
+        //             concat_begin_message("You have ", server->number_of_messages[*user_id], " short messages");
+        //             // char *ascii = itoa(server->number_of_messages[i]);
+        //             write(sock, "You have ", 10);
+        //         //    write(socket, ascii, strlen(ascii));
+        //             // write(socket, " short messages", 14);
+        //             // free(ascii);
+        //             return 0;
+        //         }
+        //     }
+        // }
+    }
+    else if (strncmp(user_input, "SEND_MESSAGE ", 13) == 0)
+    {
+		if (*user_id == -1)
+		{
+			write(sock, "You need to have an active session to send message", 51);
+			return (0);
+		}
+        char to_send[32];
+        int send_id;
+		int i = 0;
+		while (user_input[i + 13] != ' ')
+		{
+			to_send[i] = user_input[i + 13];
+			++i;
+		}
+        to_send[i] = '\0';
+        find_user(server, to_send, &send_id);
+        if (server->messages[send_id] == NULL)
+        {
+            server->messages[send_id] = malloc(sizeof(t_messages));
+            strncpy(server->messages[send_id]->message, user_input + i + 14, 224);
+            server->messages[send_id]->next = NULL;
+            strcpy(server->messages[send_id]->sending, server->users[*user_id]);
+        }
+        else
+        {
+            t_messages *current = server->messages[send_id];
+            while (current->next != NULL)
+                current = current->next;
+            current->next = malloc(sizeof(t_messages));
+            strncpy(current->next->message, user_input + i + 14, 224);
+            current->next->next = NULL;
+            strcpy(server->messages[send_id]->sending, server->users[*user_id]);
+        }
+        ++server->number_of_messages[send_id];
+        write(sock, "Your text message is received", 30);
+        return 0;
+    }
+    else if (strncmp(user_input, "GET_MESSAGE", 12) == 0)
+    {
+		if (*user_id == -1)
+		{
+			write(sock, "You need to have an active session to get message", 50);
+			return (0);
+		}
+        if (server->number_of_messages[*user_id] == 0)
+        {
+            write(sock, "You have no messages", 21);
+        }
+        else
+        {
+            t_messages *current;
+            char *response = concat_get_message(server->messages[*user_id]->sending, server->messages[*user_id]->message);
+            write(sock, response, strlen(response) + 1);
+            free(response);
+            --server->number_of_messages[*user_id];
+            current = server->messages[*user_id];
+            server->messages[*user_id] = current->next;
+            free(current);
+        }
+    }
+    else if (strncmp(user_input, "END_SESSION", 12) == 0)
+    {
+		if (*user_id == -1)
+		{
+			write(sock, "There is no active session to end", 34);
+			return (0);
+		}
+        printf("Session ended\n");
+        write(sock, "Session ended", 14);
+        *user_id = -1;
+        return 0;
+    }
+    else
+    {
+        printf("Invalid request\n");
+        write(sock, "Message Not Understood", 23);
+        return 0;
+    }
+	return (0);
+}
+
+void *client_service(void *param)
+{
+    int client_socket = ((t_server *)param)->last_client_socket;
+    t_server *server = (t_server *)param;
+    int user_id = -1;
+
+    while (1)
+    {
+        if (read_protocol(client_socket, server, &user_id))
+            break ;
+    }    
+    close(client_socket);
+    return (NULL);
+}
+
+int main()
+{
+    int server_socket; 
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket Error");
+        exit(1);
+    }
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        perror("Bind Error");
+        exit(1);
+    }
+
+    listen(server_socket, 10);
+
+    int client_socket;
+    pthread_t tid;
+    t_server server;
+    server.users = NULL;
+    server.number_of_messages = NULL;
+    server.user_count = 0;
+
+    while (1)
+    {
+        printf("Waiting for a client to connect...\n");
+        server.last_client_socket = accept(server_socket, NULL, NULL);
+        if (server.last_client_socket < 0)
+        {
+            perror("Accept Error");
+            exit(1);
+        }
+		printf("New client has been connected\n");
+        pthread_create(&tid, NULL, &client_service, &server);
+    }
+
+    close(server_socket);
+
+    return (0);
+}
+
+
